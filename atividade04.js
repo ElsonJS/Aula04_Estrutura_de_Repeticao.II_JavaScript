c04cf1f1054ee7cf28b257549e50c0e31c4c01bc
Elson Souza <107326097+ElsonJS@users.noreply.github.com>
2022-07-30 19:27:17 -0300

    Create atividade04.js

---
+++ atividade04.js
@@ -0,0 +1,10 @@
+<script>
+     //Faça um programa que calcule o mostre a média aritmética de N notas.
+      var n1=Number(prompt("Informe a quantidade  de notas:  "));    
+      var soma=0;
+
+      for(y=0; y<n1; y++){
+         soma+=Number(prompt("Informe a quantidade  de notas:  "));           
+      }
+      document.write("<b>" + soma/n1 +"</b>");
+</script>
